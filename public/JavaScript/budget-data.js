be3a7610bcf7ff980e9b3dfa00a8681ead13422f
Sebastian Petak <sebastianpetak@gmail.com>
2017-06-07 12:57:06 -0500

    Populate the budget tables with data Our budget has no way displaying the users data. Create a table for each of the 4 different types of costs in the budget.  Populate the tables with the correct data via JavaScript.

--- public/JavaScript/budget-data.js
+++ public/JavaScript/budget-data.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
+	/* Retrieve user data
+		Currently using a fake JSON file from the public folder
+		Needs to be replaced with actual api route which will use
+		the user_id to find the correct document
+	*/
 	$.ajax({
 		dataType: 'json',
 		url: 'sample-budget.json',
-	}).done(function(result) {
+	}).done(function(response) {
+		/* assign object containing a type of cost to a variable */
+		var fixedCosts = response.fixedCosts;
+		var investments = response.investments;
+		var savings = response.savings;
+		var spendingMoney = response.spendingMoney;
+
+		/* assign a string containing the name of an id.This id is assigned to
+			the <tbody> element to which we will add rows */
+		var fixedItemsId = '#fixed-cost-items';
+		var investmentItemsId = '#investment-items';
+		var savingsItemsId = '#savings-items';
+		var spendingMoneyItemsId = '#spending-money-items'
+
+		/* take in the object containing our budget items and the id to assign
+			rows to and populate the table with our users budget items */
+		var appendBudgetRows = (type, id) => {
+			$.each(type, function(i, item) {
+				$(id + '> tbody').append(
+					'<tr>',
+					$('<td>').text(item.name),
+					$('<td>').text('$' + item.amount.toFixed(2)),
+					$('<td>').text((item.amount / response.monthlyIncome * 100).toFixed(1) + '%'),
+					'</tr>'
+				);
+			});
+		};
+
+		/* call our method to populate all tables with the appropriate data */
+		appendBudgetRows(fixedCosts, fixedItemsId);
+		appendBudgetRows(investments, investmentItemsId);
+		appendBudgetRows(savings, savingsItemsId);
+		appendBudgetRows(spendingMoney, spendingMoneyItemsId);
 	});
 });
