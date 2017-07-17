9df65b10e3f253de69677d5358710954940e1107
SebastianPetak <sebastian.petak@gmail.com>
2017-07-16 22:19:11 -0500

    Begin creating updateDomBudget method Currently, the dom is updating data from a source other than the state object we created.  Begin creating a method that will update the dom based off our state object.

--- public/JavaScript/budget-data.js
+++ public/JavaScript/budget-data.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 		Needs to be replaced with actual api route which will use
 		the user_id to find the correct document
 	*/
-	// TODO Create object to contain state of the budget
 
+	// State of the budget
 	var budgetData = {
 		'fixedCosts': [],
 		'investments': [],
@@ -15,39 +15,19 @@ $(document).ready(function() {
 	}
 
 	// TODO create function to update monthlyIncome
-	// var updateMonthlyIncome = () => {
-	// 	// Post to a api route to change the monthly income
-	// }
+	var updateDomBudget = () => {
+		// Clear the DOM first
+		$('#monthlyIncome').empty();
+		$('#fixed-cost-items > tbody > tr').remove();
+		$('#investment-items > tbody > tr').remove();
+		$('#savings-items > tbody > tr').remove();
+		$('#spending-money-items > tbody > tr').remove();
+		$('#monthlyIncome > tbody > tr').remove();
 
-	// MonthlyIncome =========================================================
-	$.ajax({
-		dataType: 'json',
-		url: '/monthlyIncome',
-		type: 'GET'
-	}).done(function(response) {
-		console.log('monthlyIncome response: ' + response);
-		$('#monthlyIncome').html('Monthly Income: $' + response.monthlyIncome);
-	});
+		$('#monthlyIncome').html('Monthly Income: $' + budgetData.monthlyIncome);
 
-	// COSTS =================================================================
-	$.ajax({
-		dataType: 'json',
-		url: 'sample-budget.json',
-	}).done(function(response) {
 		/* CENTER PANE ========================================================= */
-		/* assign object containing a category of cost to a variable */
-		var fixedCosts = response.fixedCosts;
-		var investments = response.investments;
-		var savings = response.savings;
-		var spendingMoney = response.spendingMoney;
-
-		/* assign a string containing the name of an id.This id is assigned to
-			the <tbody> element to which we will add rows */
-		var fixedItemsId = '#fixed-cost-items';
-		var investmentItemsId = '#investment-items';
-		var savingsItemsId = '#savings-items';
-		var spendingMoneyItemsId = '#spending-money-items';
-
+		/* assign object containing a category of cost to a state */
 		/* take in the object containing our budget items and the id to assign
 			rows to and populate the table with our users budget items */
 		var appendBudgetRows = (category, id) => {
@@ -56,21 +36,19 @@ $(document).ready(function() {
 					'<tr>',
 					$('<td>').text(item.name),
 					$('<td>').text('$' + item.amount.toFixed(2)),
-					$('<td>').text((item.amount / response.monthlyIncome * 100).toFixed(1) + '%'),
+					$('<td>').text((item.amount / budgetData.monthlyIncome * 100).toFixed(1) + '%'),
 					'</tr>'
 				);
 			});
 		};
 
 		/* call our function to populate all tables with the appropriate data */
-		appendBudgetRows(fixedCosts, fixedItemsId);
-		appendBudgetRows(investments, investmentItemsId);
-		appendBudgetRows(savings, savingsItemsId);
-		appendBudgetRows(spendingMoney, spendingMoneyItemsId);
+		appendBudgetRows(budgetData.fixedCosts, '#fixed-cost-items');
+		appendBudgetRows(budgetData.investments, '#investment-items');
+		appendBudgetRows(budgetData.savings, '#savings-items');
+		appendBudgetRows(budgetData.spendingMoney,'#spending-money-items');
 
 		/* LEFT PANE =========================================================== */
-		/* display total monthly income */
-		// $('#monthlyIncome').html('Monthly Income: $' + response.monthlyIncome);
 		/* calculate and display how much of the monthly income is used by each
 			category */
 		var calculateProgressBar = (category, id) => {
@@ -90,12 +68,111 @@ $(document).ready(function() {
 			}
 			// Find the percentage of the monthly income that the category uses,
 			// and set the width of the progress bar to that percent.
-			$(id).css('width', (categoryCost / response.monthlyIncome * 100).toFixed(1) + '%');
+			$(id).css('width', (categoryCost / budgetData.monthlyIncome * 100).toFixed(1) + '%');
 		};
 		// Call our function to advance the progress bars
-		calculateProgressBar(fixedCosts, '#fixed-costs-overview .bar-progress');
-		calculateProgressBar(investments, '#investment-overview .bar-progress');
-		calculateProgressBar(savings, '#savings-overview .bar-progress');
-		calculateProgressBar(spendingMoney, '#spending-money-overview .bar-progress');
-	});
+		calculateProgressBar(budgetData.fixedCosts, '#fixed-costs-overview .bar-progress');
+		calculateProgressBar(budgetData.investments, '#investment-overview .bar-progress');
+		calculateProgressBar(budgetData.savings, '#savings-overview .bar-progress');
+		calculateProgressBar(budgetData.spendingMoney, '#spending-money-overview .bar-progress');
+	}
+
+	// MonthlyIncome =========================================================
+	$.ajax({
+		dataType: 'json',
+		url: '/monthlyIncome',
+		type: 'GET'
+	}).done(function(response) {
+		budgetData.monthlyIncome = response.monthlyIncome;
+	}).then(function() {
+		updateDomBudget();
+	}).catch(function(err) {
+		console.log(err);
+	})
+
+	// COSTS =================================================================
+	$.ajax({
+		dataType: 'json',
+		url: 'sample-budget.json',
+	}).done(function(response) {
+		budgetData.fixedCosts = response.fixedCosts;
+		budgetData.investments = response.investments;
+		budgetData.savings = response.savings;
+		budgetData.spendingMoney = response.spendingMoney;
+	}).then(function() {
+		updateDomBudget();
+	}).catch(function(err) {
+		console.log(err);
+	})
+
+
+	// // Old budget ajax call
+	// $.ajax({
+	// 	dataType: 'json',
+	// 	url: 'sample-budget.json',
+	// }).done(function(response) {
+	// 	/* CENTER PANE ========================================================= */
+	// 	/* assign object containing a category of cost to a variable */
+	// 	var fixedCosts = response.fixedCosts;
+	// 	var investments = response.investments;
+	// 	var savings = response.savings;
+	// 	var spendingMoney = response.spendingMoney;
+	//
+	// 	/* assign a string containing the name of an id.This id is assigned to
+	// 		the <tbody> element to which we will add rows */
+	// 	var fixedItemsId = '#fixed-cost-items';
+	// 	var investmentItemsId = '#investment-items';
+	// 	var savingsItemsId = '#savings-items';
+	// 	var spendingMoneyItemsId = '#spending-money-items';
+	//
+	// 	/* take in the object containing our budget items and the id to assign
+	// 		rows to and populate the table with our users budget items */
+	// 	var appendBudgetRows = (category, id) => {
+	// 		$.each(category, function(i, item) {
+	// 			$(id + '> tbody').append(
+	// 				'<tr>',
+	// 				$('<td>').text(item.name),
+	// 				$('<td>').text('$' + item.amount.toFixed(2)),
+	// 				$('<td>').text((item.amount / response.monthlyIncome * 100).toFixed(1) + '%'),
+	// 				'</tr>'
+	// 			);
+	// 		});
+	// 	};
+	//
+	// 	/* call our function to populate all tables with the appropriate data */
+	// 	appendBudgetRows(fixedCosts, fixedItemsId);
+	// 	appendBudgetRows(investments, investmentItemsId);
+	// 	appendBudgetRows(savings, savingsItemsId);
+	// 	appendBudgetRows(spendingMoney, spendingMoneyItemsId);
+	//
+	// 	/* LEFT PANE =========================================================== */
+	// 	/* display total monthly income */
+	// 	// $('#monthlyIncome').html('Monthly Income: $' + response.monthlyIncome);
+	// 	/* calculate and display how much of the monthly income is used by each
+	// 		category */
+	// 	var calculateProgressBar = (category, id) => {
+	// 		var categoryCost;
+	// 		// If category has no costs, bar progress width to 0
+	// 		if (category.length === 0) {
+	// 			categoryCost = 0;
+	// 		}
+	// 		//  If category only has one cost, use it's amount
+	// 		else if (category.length === 1) {
+	// 			categoryCost = category[0].amount;
+	// 			// If category has more than one cost, add the costs together.
+	// 		} else {
+	// 			categoryCost = category.reduce((a, b) => {
+	// 				return a.amount + b.amount;
+	// 			});
+	// 		}
+	// 		// Find the percentage of the monthly income that the category uses,
+	// 		// and set the width of the progress bar to that percent.
+	// 		$(id).css('width', (categoryCost / response.monthlyIncome * 100).toFixed(1) + '%');
+	// 	};
+	// 	// Call our function to advance the progress bars
+	// 	calculateProgressBar(fixedCosts, '#fixed-costs-overview .bar-progress');
+	// 	calculateProgressBar(investments, '#investment-overview .bar-progress');
+	// 	calculateProgressBar(savings, '#savings-overview .bar-progress');
+	// 	calculateProgressBar(spendingMoney, '#spending-money-overview .bar-progress');
+	// });
 });
